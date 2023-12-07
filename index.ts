9d0234c7d02c14417d9eef0c33ff391e1d0e311f
ig <indghosh12@gmail.com>
2023-12-07 09:01:51 +0530

    initial commit

---
+++ index.ts
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./src/components/App";
+
+const RDom = ReactDOM.createRoot(document.getElementById("root"));
+RDom.render(App());
